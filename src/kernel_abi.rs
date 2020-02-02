63fb06b79891b96195d036b26a47c90d8df9b168
Sidharth Kshatriya <sid.kshatriya@gmail.com>
2020-02-02 16:28:37 +0530

    struct siginfo_t for kernel_abi

--- src/kernel_abi.rs
+++ src/kernel_abi.rs
@@ -3,7 +3,6 @@
 use crate::bindings::kernel;
 use crate::remote_ptr::RemotePtr;
 use std::convert::TryInto;
-use std::ffi::c_void;
 use std::marker::PhantomData;
 
 #[derive(Copy, Clone)]
@@ -55,12 +54,14 @@ trait Architecture {
     type msghdr: Default + Copy + Clone;
     type cmsghdr: Default + Copy + Clone;
     type iovec: Default + Copy + Clone;
+    type siginfo_t: Copy + Clone;
 }
 
 impl Architecture for X86Arch {
     type msghdr = msghdr_x86;
     type cmsghdr = cmsghdr_x86;
     type iovec = iovec_x86;
+    type siginfo_t = siginfo_t_x86;
 }
 
 #[cfg(target_arch = "x86_64")]
@@ -68,6 +69,7 @@ impl Architecture for X8664Arch {
     type msghdr = msghdr_x86_64;
     type cmsghdr = cmsghdr_x86_64;
     type iovec = iovec_x86_64;
+    type siginfo_t = siginfo_t_x86_64;
 }
 
 ///////////////////// Ptr
@@ -200,3 +202,121 @@ assert_eq_size!(kernel::iovec, iovec_x86_64);
 assert_eq_align!(kernel::iovec, iovec_x86);
 #[cfg(target_arch = "x86")]
 assert_eq_size!(kernel::iovec, iovec_x86);
+
+///////////////////// siginfo_t
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct kill {
+    si_pid_: i32,
+    si_uid_: u32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct timer<P: Copy + Clone> {
+    si_tid_: i32,
+    si_overrun_: i32,
+    si_sigval_: sigval_t<P>,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub union sigval_t<P: Copy + Clone> {
+    sival_int: i32,
+    sival_ptr: P,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct rt<P: Copy + Clone> {
+    si_pid_: i32,
+    si_uid_: u32,
+    si_sigval_: sigval_t<P>,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct sigchld<SLongT: Copy + Clone> {
+    si_pid_: i32,
+    si_uid_: u32,
+    si_status_: i32,
+    si_utime_: SLongT,
+    si_stime_: SLongT,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct sigfault<P: Copy + Clone> {
+    si_addr_: P,
+    si_addr_lsb_: i16,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct sigpoll<SLongT: Copy + Clone> {
+    si_band_: SLongT,
+    si_fd_: i32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct sigsys<P: Copy + Clone> {
+    _call_addr: P,
+    _syscall: i32,
+    _arch: u32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub union sifields_x86 {
+    padding: [i32; 29], // 32: 128/4 - 3 = 29,
+    _kill: kill,
+    _timer: timer<PtrX86<u8>>,
+    _rt: rt<PtrX86<u8>>,
+    _sigchld: sigchld<i32>,
+    _sigfault: sigfault<PtrX86<u8>>,
+    _sigpoll: sigpoll<i32>,
+    _sigsys: sigsys<PtrX86<u8>>,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub union sifields_x86_64 {
+    padding: [i32; 28], // 64: 128/4 - 4 = 28
+    _kill: kill,
+    _timer: timer<PtrX8664<u8>>,
+    _rt: rt<PtrX8664<u8>>,
+    _sigchld: sigchld<i64>,
+    _sigfault: sigfault<PtrX8664<u8>>,
+    _sigpoll: sigpoll<i64>,
+    _sigsys: sigsys<PtrX8664<u8>>,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct siginfo_t_x86 {
+    si_signo: i32,
+    si_errno: i32,
+    si_code: i32,
+    _sifields: sifields_x86,
+}
+
+#[cfg(target_arch = "x86_64")]
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct siginfo_t_x86_64 {
+    si_signo: i32,
+    si_errno: i32,
+    si_code: i32,
+    _sifields: sifields_x86_64,
+}
+
+#[cfg(target_arch = "x86_64")]
+assert_eq_align!(kernel::siginfo_t, siginfo_t_x86_64);
+#[cfg(target_arch = "x86_64")]
+assert_eq_size!(kernel::siginfo_t, siginfo_t_x86_64);
+
+#[cfg(target_arch = "x86")]
+assert_eq_align!(kernel::siginfo_t, siginfo_t_x86);
+#[cfg(target_arch = "x86")]
+assert_eq_size!(kernel::siginfo_t, siginfo_t_x86);
