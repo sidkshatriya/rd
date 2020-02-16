46d4e568071e63501f08bacdefed965227b58a0b
Sidharth Kshatriya <sid.kshatriya@gmail.com>
2020-02-16 13:00:24 +0530

    dont need to blacklist struct timex with bindgen 0.53.1. binding compiles

--- build.rs
+++ build.rs
@@ -59,6 +59,8 @@ fn main() {
     let perf_event_bindings = Builder::default()
         .parse_callbacks(Box::new(CargoCallbacks))
         .prepend_enum_name(false)
+        // Workaround for "error[E0587]: type has conflicting packed and align representation hints"
+        // We don't need these types so just omit them.
         .blacklist_type("perf_event_mmap_page")
         .blacklist_type("perf_event_mmap_page__bindgen_ty_1__bindgen_ty_1")
         .blacklist_type("perf_event_mmap_page__bindgen_ty_1")
@@ -83,7 +85,6 @@ fn main() {
 
     let kernel_abi_bindings = Builder::default()
         .parse_callbacks(Box::new(CargoCallbacks))
-        .blacklist_type("timex")
         .prepend_enum_name(false)
         .header("bindgen/kernel_wrapper.h")
         .generate()
