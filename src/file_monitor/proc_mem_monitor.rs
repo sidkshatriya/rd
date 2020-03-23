3005da67130e3f7f36fda188df1dbbf86e8fb2eb
Sidharth Kshatriya <sid.kshatriya@gmail.com>
2020-03-23 11:55:46 +0530

    ProcMemMonitor::new()

--- src/file_monitor/proc_mem_monitor.rs
+++ src/file_monitor/proc_mem_monitor.rs
@@ -3,15 +3,53 @@ use crate::file_monitor::{FileMonitor, FileMonitorType, LazyOffset, Range};
 use crate::remote_ptr::RemotePtr;
 use crate::task::Task;
 use crate::taskish_uid::TaskUid;
+use libc::pid_t;
 use std::convert::TryInto;
+use std::ffi::OsStr;
+use std::os::unix::ffi::OsStrExt;
+use std::path::{Component, Path};
 
 pub struct ProcMemMonitor {
     maybe_tuid: Option<TaskUid>,
 }
 
+impl ProcMemMonitor {
+    pub fn new(t: &dyn Task, pathname: &Path) -> ProcMemMonitor {
+        let mut components = pathname.components();
+        let maybe_rootdir = components.next();
+        let maybe_proc = components.next();
+        let maybe_tid_os_str = components.next();
+        let maybe_mem = components.next();
+        if (maybe_rootdir, maybe_proc, maybe_mem)
+            == (
+                Some(Component::RootDir),
+                Some(Component::Normal(OsStr::new("proc"))),
+                Some(Component::Normal(OsStr::new("mem"))),
+            )
+        {
+            match maybe_tid_os_str {
+                Some(Component::Normal(tid_os_str)) => {
+                    let tid_str = String::from_utf8_lossy(tid_os_str.as_bytes());
+                    let maybe_tid = tid_str.parse::<pid_t>();
+                    let tid = maybe_tid.unwrap();
+                    let maybe_found = t
+                        .session()
+                        .borrow()
+                        .find_task_from_rec_tid(tid)
+                        .map_or(None, |ft| Some(ft.tuid()));
+                    return ProcMemMonitor {
+                        maybe_tuid: maybe_found,
+                    };
+                }
+                _ => (),
+            }
+        }
+        ProcMemMonitor { maybe_tuid: None }
+    }
+}
 impl FileMonitor for ProcMemMonitor {
     fn file_monitor_type(&self) -> FileMonitorType {
-        unimplemented!()
+        FileMonitorType::ProcMem
     }
 
     /// We need to PREVENT_SWITCH, since the timing of the write is otherwise
