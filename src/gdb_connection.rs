5f4396e5b186cdb61b66ae779a0a7542a6c30192
Sidharth Kshatriya <sid.kshatriya@gmail.com>
2020-09-10 16:36:47 +0530

    GdbConnection struct

--- src/gdb_connection.rs
+++ src/gdb_connection.rs
@@ -1,4 +1,5 @@
-use crate::{gdb_register::GdbRegister, registers::MAX_REG_SIZE_BYTES};
+use crate::{gdb_register::GdbRegister, registers::MAX_REG_SIZE_BYTES, scoped_fd::ScopedFd};
+use libc::pid_t;
 
 /// Represents a possibly-undefined register `name`.  `size` indicates how
 /// many bytes of `value` are valid, if any.
@@ -50,4 +51,52 @@ impl GdbRegisterValue {
             _ => panic!("Unexpected GdbRegisterValue: {:?}", self),
         }
     }
+}
+
+/// @TODO
+pub struct GdbThreadId;
+
+/// @TODO
+pub struct GdbRequest;
+
+pub struct GdbConnectionFeatures {
+    reverse_execution: bool,
+}
+
+impl Default for GdbConnectionFeatures {
+    fn default() -> Self {
+        Self {
+            reverse_execution: true,
+        }
+    }
+}
+
+/// This struct wraps up the state of the gdb protocol, so that we can
+/// offer a (mostly) stateless interface to clients.
+pub struct GdbConnection {
+    /// Current request to be processed.
+    req: GdbRequest,
+    /// Thread to be resumed.
+    resume_thread: GdbThreadId,
+    /// Thread for get/set requests.
+    query_thread: GdbThreadId,
+    /// gdb and rd don't work well together in multi-process and
+    /// multi-exe-image debugging scenarios, so we pretend only
+    /// this thread group exists when interfacing with gdb
+    tgid: pid_t,
+    cpu_features_: u32,
+    /// true when "no-ack mode" enabled, in which we don't have
+    /// to send ack packets back to gdb.  This is a huge perf win.
+    no_ack: bool,
+    sock_fd: ScopedFd,
+    /// buffered input from gdb
+    inbuf: Vec<u8>,
+    /// index of '#' character
+    packetend: usize,
+    /// buffered output from gdb
+    outbuf: Vec<u8>,
+    features_: GdbConnectionFeatures,
+    connection_alive_: bool,
+    /// client supports multiprocess extension
+    multiprocess_supported_: bool,
 }
