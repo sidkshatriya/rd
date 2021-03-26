a59ff8587bf83206103ead124f71ea17e7e943d3
Sidharth Kshatriya <sid.kshatriya@gmail.com>
2021-03-27 03:47:32 +0530

    Incorporate "Make failing to dup the desched fd non-fatal"
    From rr commit:
    ---
    commit 7254a89d1ab7919c8d92d26a19e0a24afd4f4cf1
    Author: Keno Fischer <keno@juliacomputing.com>
    Date:   Tue Apr 21 01:48:22 2020 -0400
    
        Make failing to dup the desched fd non-fatal
    
        We try to avoid using the low fds to avoid disrupting the tracee (e.g.
        the tracee may want to reserve a portion of fd space for its exclusive
        management and get confused if we put an fd there). However, if we can't
        use a high fd (e.g. because of a ulimit), then this shouldn't be fatal,
        since it's not a problem if the tracee doesn't indiscriminately close
        file descriptors. We could probably do better in the future, but for
        now, let's just close the immediate failure mode with low ulimits.
    
        Part of #2521
    ---

--- src/preload/syscallbuf.c
+++ src/preload/syscallbuf.c
@@ -452,11 +452,15 @@ static int open_desched_event_counter(size_t nr_descheds, pid_t tid) {
   }
   fd = privileged_traced_fcntl(tmp_fd, F_DUPFD_CLOEXEC,
                                RR_DESCHED_EVENT_FLOOR_FD);
-  if (0 > fd) {
-    fatal("Failed to dup desched fd");
-  }
-  if (privileged_untraced_close(tmp_fd)) {
-    fatal("Failed to close tmp_fd");
+  if (fd > 0) {
+    if (privileged_untraced_close(tmp_fd)) {
+      fatal("Failed to close tmp_fd");
+    }
+  } else {
+    // We may be unable to find an fd above the RR_DESCHED_EVENT_FLOOR_FD (e.g
+    // because of a low ulimit). In that case, just use the tmp_fd we already
+    // have.
+    fd = tmp_fd;
   }
   if (privileged_untraced_fcntl(fd, F_SETFL, O_ASYNC)) {
     fatal("Failed to fcntl(O_ASYNC) the desched counter");
