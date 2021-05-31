631e39882a4ca6b282973cf8f387de932f991ffe
Sidharth Kshatriya <sid.kshatriya@gmail.com>
2021-05-31 15:18:41 +0530

    lexical_key changes

--- src/trace/lexical_key.rs
+++ src/trace/lexical_key.rs
@@ -1,23 +1,13 @@
 use crate::util::u8_slice;
 
 #[repr(C)]
-struct LexicalKey128 {
+#[derive(Copy, Clone)]
+pub struct LexicalKey128 {
     key1: [u8; 8],
     key2: [u8; 8],
 }
 
-#[repr(C)]
-struct LexicalKey64 {
-    key: [u8; 8],
-}
-
 impl AsRef<[u8]> for LexicalKey128 {
-    fn as_ref(&self) -> &[u8] {
-        u8_slice(self)
-    }
-}
-
-impl AsRef<[u8]> for LexicalKey64 {
     fn as_ref(&self) -> &[u8] {
         u8_slice(self)
     }
@@ -30,12 +20,12 @@ impl LexicalKey128 {
             key2: key2.to_be_bytes(),
         }
     }
-}
 
-impl LexicalKey64 {
-    pub fn new(key: u64) -> LexicalKey64 {
-        LexicalKey64 {
-            key: key.to_be_bytes(),
-        }
+    pub fn key1(&self) -> u64 {
+        u64::from_be_bytes(self.key1)
+    }
+
+    pub fn key2(&self) -> u64 {
+        u64::from_be_bytes(self.key2)
     }
 }
